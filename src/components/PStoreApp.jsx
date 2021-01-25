ecd1218ab620ab2d509bc9945e550d0c48bfafc9
popuri <popuri.prathima@gmail.com>
2021-01-24 23:16:52 -0600

    initial commit

---
+++ src/components/PStoreApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function pStoreApps1(){
+    return <h1> " welcome to PStore1" </h1>
+}
+
+function pStoreApps(){
+    return <h1> " welcome to pStoreApps" </h1>
+}
+
+ export default pStoreApps;
